23b3809b9e3f3917be6987ea0e251ff17d581b24
Deena Lim <deena.lsy7@gmail.com>
2024-04-01 17:10:32 +0800

    Update react to 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./app";
 import { Auth0ProviderWithHistory } from "./auth0-provider-with-history";
 import "./styles/styles.css";
 
-ReactDOM.render(
+// ReactDOM.render(
+//   ,
+//   document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Auth0ProviderWithHistory>
         <App />
       </Auth0ProviderWithHistory>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
